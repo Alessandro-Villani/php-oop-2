35d8ddad5cf29ffd456964ab248fbba5e5d2172e
Alessandro-Villani <alessvill@libero.it>
2023-02-20 16:19:08 +0100

    added animal object

--- Models/Animal.php
+++ Models/Animal.php
@@ -2,4 +2,32 @@
 
 class Animal
 {
+    private $name;
+    private $size;
+
+    public function __construct(string $_name, string $_size)
+    {
+        $this->setName($_name);
+        $this->setSize($_size);
+    }
+
+    public function setName($name)
+    {
+        if (is_numeric($name) || !$name) return false;
+        $this->name = $name;
+        return true;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setSize($size)
+    {
+        $allowed_sizes = ['piccola', 'media', 'grande', 'gigante'];
+        if (!in_array($size, $allowed_sizes)) return false;
+        $this->size = $size;
+        return true;
+    }
 }
